a4e71d6d630c138348a763081fcad7ea04154d3e
ikramhasib007 <ikramhasib007@gmail.com>
2022-12-10 18:48:05 +0600

    class declaration

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,15 @@
-console.log('under goning...')
+class Department {
+  name: string;
+
+  constructor(n: string) {
+    this.name = n;
+  }
+  describe(this: Department) { // additional safety purposes
+    console.log('Department is ', this.name);
+  }
+}
+
+const accounting = new Department('Accounting');
+accounting.describe()
+const accountingCopy = { name: 'New', describe: accounting.describe }
+accountingCopy.describe()
