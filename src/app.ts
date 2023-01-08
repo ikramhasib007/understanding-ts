96eb4ec61db2d2e0c024f7b2afda37ed13bf7475
ikramhasib007 <ikramhasib007@gmail.com>
2023-01-08 14:34:46 +0600

    more useful with decorators

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,18 @@ function Logger(loggerMessge: string) {
   }
 }
 
-@Logger('logger messages...')
+function WithTemplate(template: string, hookId: string) {
+  return function(constructor: any) {
+    const p = new constructor()
+    const hookEl = document.getElementById(hookId)!
+    hookEl.innerHTML = template;
+    document.querySelector('h1')!.innerText = p.name;
+  }
+}
+
+
+// @Logger('logger messages...')
+@WithTemplate('<h1>My customized title</h1>', 'app')
 class Person {
   name: string = 'Max';
 
