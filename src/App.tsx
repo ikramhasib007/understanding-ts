7f84f329ac7dfeb14f15f3b3b711a9e0f1897df6
ikramhasib007 <ikramhasib007@gmail.com>
2023-01-09 02:42:58 +0600

    handler function added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import NewTodo from './components/NewTodo';
 import TodoList from './components/TodoList';
 
 function App() {
   const todos = [{ id: 't1', text: 'Just finished!' }]
+
+  const handleAddTodo = (newTodo: string) => {
+    console.log('newTodo: ', newTodo);
+
+  }
+
   return (
     <div className="App">
+      <NewTodo onAddTodo={handleAddTodo} />
       <TodoList items={todos} />
     </div>
   );
---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,24 @@
+import React, { useRef } from 'react'
+
+type NewTodoProps = {
+  onAddTodo: (T: string) => void
+}
+
+const NewTodo: React.FC<NewTodoProps> = (props) => {
+  const textRef = useRef<HTMLInputElement>(null);
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    const enteredText = textRef.current!.value;
+    props.onAddTodo(enteredText)
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="text-input" />
+      <input type="text" id='text-input' ref={textRef} />
+      <button type="submit">Add</button>
+    </form>
+  )
+}
+
+export default NewTodo;
